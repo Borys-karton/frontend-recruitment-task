fc5576aa6a146c1d4b47c5f256d8e2cfa8a71a7e
Borys-karton <59846312+Borys-karton@users.noreply.github.com>
2022-05-23 16:23:35 +0200

    Javascript functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
-console.log("test");
+const openAlert = document.querySelector("#openAlertBtn");
+const alertWindow = document.querySelector("#alertWindow");
+const clickCountDisplay = document.querySelector("#clickCount");
+const resetBtn = document.querySelector("#resetCountBtn");
+const closeBtn = document.querySelector("#closeButton");
+
+openAlert.onclick =() =>{
+    alertWindow.style.display = "block";
+    if(localStorage.hasOwnProperty("clickCount")){
+        localStorage.clickCount = Number(localStorage.clickCount)+1;
+        if(localStorage.clickCount >= 5){
+            resetBtn.style.display = "block"; 
+        }
+    }else{
+        localStorage.clickCount = 1;
+    }
+    refreshDisplay();
+};
+closeBtn.onclick =()=>{closeAlert()};
+window.onclick = (event)=>{if(event.target == alertWindow)closeAlert()};
+resetBtn.onclick =()=>{
+    localStorage.removeItem("clickCount");
+    resetBtn.style.display = "none";
+    refreshDisplay();
+}
+function refreshDisplay(){
+    if(localStorage.hasOwnProperty("clickCount")){
+        clickCountDisplay.innerHTML = localStorage.clickCount;
+    }else{
+        clickCountDisplay.innerHTML = 0;
+    }
+}
+function closeAlert(){
+    alertWindow.style.display = "none";
+}
