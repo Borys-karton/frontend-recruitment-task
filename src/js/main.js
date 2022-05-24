cfef55eafb2d8353ac3022b1740c4db32fb913bd
Borys-karton <59846312+Borys-karton@users.noreply.github.com>
2022-05-24 09:57:13 +0200

    additional task

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,25 @@ const alertWindow = document.querySelector("#alertWindow");
 const clickCountDisplay = document.querySelector("#clickCount");
 const resetBtn = document.querySelector("#resetCountBtn");
 const closeBtn = document.querySelector("#closeButton");
+const table = document.querySelector("#table");
+const tableData = document.querySelector("#tableData");
+const loadBar = document.querySelector("#loadBar");
+const alertContent = document.querySelector("#alertContent");
+
+const importDataURL = "https://jsonplaceholder.typicode.com/users";
+
+const closeAlert=()=>{
+    alertWindow.style.display = "none";
+}
+const refreshDisplay=()=>{
+    if(localStorage.hasOwnProperty("clickCount")){
+        clickCountDisplay.innerHTML = localStorage.clickCount;
+    }else{
+        clickCountDisplay.innerHTML = 0;
+    }
+}
+
+
 
 openAlert.onclick =() =>{
     alertWindow.style.display = "block";
@@ -23,13 +42,60 @@ resetBtn.onclick =()=>{
     resetBtn.style.display = "none";
     refreshDisplay();
 }
-function refreshDisplay(){
-    if(localStorage.hasOwnProperty("clickCount")){
-        clickCountDisplay.innerHTML = localStorage.clickCount;
-    }else{
-        clickCountDisplay.innerHTML = 0;
+const fetchData = async () =>{
+    try {
+        const response = await fetch(importDataURL);
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+      }
+      catch(error) {
+        console.error(`Could not get data: ${error}`);
+      }
+};
+const fillTableWithContent = async () =>{
+    let errored=false;
+    try{
+        loadBar.style.display = "block";
+       const data = await fetchData();
+       data.forEach(user => {
+           let row = document.createElement('tr');
+
+           let name = document.createElement('td');
+           let email = document.createElement('td');
+           let address = document.createElement('td');
+           let phone = document.createElement('td');
+           let company = document.createElement('td');
+
+           name.innerHTML = user.name;
+           email.innerHTML = user.email;
+           address.innerHTML = user.address.city + " " + user.address.street + " " + user.address.suite;
+           phone.innerHTML = user.phone;
+           company.innerHTML = user.company.name;
+           row.appendChild(name);
+           row.appendChild(email);
+           row.appendChild(address);
+           row.appendChild(phone);
+           row.appendChild(company);
+
+           tableData.appendChild(row);
+       });
     }
-}
-function closeAlert(){
-    alertWindow.style.display = "none";
-}
+    catch(error){
+        console.error(`An error occured: ${error.message}`)
+        errored = true;
+        let errorMsg = document.createElement('p');
+        errorMsg.style.color = "#F00";
+        errorMsg.style.fontWeight = "700";
+        errorMsg.innerHTML = "Something went wrong. Check console for errors.";
+        alertContent.appendChild(errorMsg);
+    }finally{
+        loadBar.style.display = "none";
+        if(!errored){
+            table.style.display = "block";
+        }
+    }
+};
+fillTableWithContent();
